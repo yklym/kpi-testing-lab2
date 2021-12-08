338612c15422cc1d83a832dac170ed00a34d6165
yklym <yar.klymko@gmail.com>
2021-12-08 17:56:21 +0200

    finished task 2, bugfix

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
+from entities import Dir, Bin, Buff, Log
+# from helpers.tree import move_file
 
 
 def main():
+    root = Dir('root')
+
+    dir_one = Dir('dir_one')
+    dir_two = Dir('dir_two')
+
+    root.insert_node(dir_one)
+    root.insert_node(dir_two)
+
+    dir_one.insert_node(Buff('buffer_one'))
+    dir_one.insert_node(Log('log.txt'))
+    dir_one.insert_node(Bin('bin', 'some content'))
+
+    dir_two.insert_node(Buff('buffer_two'))
+    dir_two.insert_node(Log('another_log.txt'))
+    dir_two.insert_node(Bin('bin', 'some content 2'))
+
+    print('ROOT folder:')
+    root.list_children()
+
+    print('\nFOLDER ONE')
+    dir_one.list_children()
+
+    print('\nFOLDER TWO')
+    dir_two.list_children()
+
     return 0
+
 
 if __name__ == '__main__':
     main()
